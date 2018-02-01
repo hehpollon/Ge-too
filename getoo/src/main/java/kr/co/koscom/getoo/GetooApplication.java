b43f31ee86db5c00a37cc1348ec733f39b14ee22
hehpollon <hehpollon@gmail.com>
2018-02-01 14:01:34 +0900

    init

---
+++ getoo/src/main/java/kr/co/koscom/getoo/GetooApplication.java
@@ -0,0 +1,12 @@
+package kr.co.koscom.getoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetooApplication.class, args);
+	}
+}
